34aee3916510d7e17aeb9cb4a995207805fb6426
trpill <baurin.lg@gmail.com>
2017-03-02 20:26:11 +0100

    Create setup.py
    add setup.py to export to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'stationdiy',
+  packages = ['stationdiy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Baurin Leza',
+  author_email = 'baurin.lg@gmail.com',
+  url = 'https://github.com/trpill/stationdiy', # use the URL to the github repo
+  download_url = 'https://github.com/trpill/stationdiy/new/master/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['stationdiy', 'iot', 'internet'], #  keywords
+  classifiers = [],
+)
